5b2a6c0b41c1f091d23c04a290d416224a815b0a
Zia Beg <ziabeg@hotmail.com>
2025-06-26 15:05:48 +0200

    FINAL Final Comming

--- update_robot_position.py
+++ update_robot_position.py
@@ -3,7 +3,6 @@ from ev3dev2.motor import *
 from ev3dev2.sound import Sound
 from time import sleep
 
-# CODE FOR MANUAL TESTING
 
 HOST = ''  # Listen on all interfaces
 PORT = 9999
@@ -79,43 +78,3 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             if not data:
                 exit()
             handle_command(data)
-
-# ACTUAL CODE FOR MOVING TO BALLS
-
-# def move_to_target(angle, seconds):
-
-#     print("Moving to target with angle: {} degrees".format(angle))
-
-#     spkr = Sound()
-
-#     # spkr.set_volume(100)
-#     # spkr.speak("Moving to target")
-
-#     tank = MoveTank(OUTPUT_A, OUTPUT_C)
-#     tank.gyro = GyroSensor()
-#     tank.gyro.calibrate()
-
-#     port = MediumMotor(OUTPUT_D)
-#     shooter = MediumMotor(OUTPUT_B)
-    
-#     tank.turn_degrees(speed=SpeedPercent(5), target_angle=angle)
-
-#     tank.on_for_seconds(left_speed=30, right_speed=30, seconds=seconds)
-
-#     port.on_for_seconds(speed=10, seconds=1)
-#     tank.on_for_seconds(left_speed=10, right_speed=10, seconds=2)
-#     port.on_for_seconds(speed=-10, seconds=1)
-
-#     exit()
-
-# # Safely parse command-line args
-# if len(sys.argv) < 3:
-#     print("Usage: python3 update_robot_position.py <x> <y>")
-#     sys.exit(1)
-
-# try:
-#     angle = int(sys.argv[1])
-#     seconds = float(sys.argv[2])
-#     move_to_target(angle, seconds)
-# except ValueError:
-#     print("Invalid coordinates. Must be integers.")
